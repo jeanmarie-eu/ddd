32a763623f07f320a900d57b64ce17d95fa2e67c
jeanmarielepioufle <jm@nexmodeling.com>
2016-10-19 19:40:59 +0200

    add modelArea instead of area

--- R/init.groundwater.R
+++ R/init.groundwater.R
@@ -1,12 +1,15 @@
 #' init
 
 #' The funtion initializes the main features of the groundwater:
-#' @param method method for the initialization, "load", "source", "manual", "processed"
-#' @param path directory where to get the files, in used when method is "load" or "source"
+#' @param method method for the initialization, "load", "source", "manual"
+#' @param Magkap Magkap
+#' @param M  Groundwater Storage Capacity (GSC)
+#' @param Layers saturation layers
 #' @param Timeresinsec time resolution of the process in second (1hour: 3600s, ... etc)
 #' @param UHMAD Unit Hydrograph of the Mean Annual Discharge
 #' @param MAD value of the Mean Annual Discharge
-#' @param area in squared meters
+#' @param modelArea
+#' list(totarea,slopesriverarea,nobognoglacarea,bogarea)
 #' @param modelSaturation list of parameters about the saturation
 #'  list(gtcel,CapacityUpperLevel,mLam,varLam,distr)
 #' @param modelLayer list of parameters about the Layers
@@ -18,9 +21,9 @@
 #' init.groundWater()
 #' }
 
-init.groundWater <-function(method=NULL,path=NULL,Timeresinsec=NULL,UHMAD=NULL,MAD=NULL,area=NULL,modelSaturation=NULL,modelLayer=NULL){
+init.groundWater <-function(method=NULL,path=NULL,Timeresinsec=NULL,UHMAD=NULL,MAD=NULL,modelArea=NULL,modelSaturation=NULL,modelLayer=NULL){
 
-  res <- dddGroundwater::init.groundwater(method=method,path=path,Timeresinsec=Timeresinsec,UHMAD=UHMAD,MAD=MAD,area=area,modelSaturation=modelSaturation,modelLayer=modelLayer)
+  res <- dddGroundwater::init.groundwater(method=method,path=path,Timeresinsec=Timeresinsec,UHMAD=UHMAD,MAD=MAD,modelArea=modelArea,modelSaturation=modelSaturation,modelLayer=modelLayer)
 
   return(res)
 }
