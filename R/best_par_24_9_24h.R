90c87e3c37c4dd9f603dd531721643ead0743f7b
jeanmarielepioufle <jm@nexmodeling.com>
2016-11-15 18:35:31 +0100

    add datasets of a real case study

---
+++ R/best_par_24_9_24h.R
@@ -0,0 +1,11 @@
+#' Set of parameters for a daily simulation at Tingvatn
+#'
+#' Set of parameters for a daily simulation at Tingvatn
+#'
+#' @docType data
+#' @name best_par_24.9_24h
+#' @format A txt file
+#' @keywords datasets
+#' @references the Norwegian Meteorological Institute (met.no)
+#' @source \href{http://www.met.no}{met.no}
+NULL
---
+++ R/val_24.9_24hptq_kal.R
@@ -0,0 +1,11 @@
+#' Dataset of daily variables ptq at Tingvatn
+#'
+#' Dataset of daily variables ptq at Tingvatn
+#'
+#' @docType data
+#' @name val_24.9_24hptq_kal
+#' @format A txt file
+#' @keywords datasets
+#' @references the Norwegian Meteorological Institute (met.no)
+#' @source \href{http://www.met.no}{met.no}
+NULL
