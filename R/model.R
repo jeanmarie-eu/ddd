12ff08efbb27c8f1323a473dbc790114b5ed2d06
jeanmarielepioufle <jm@nexmodeling.com>
2016-11-18 11:29:55 +0100

    more elasticity in the choice of the arguments

--- R/model.R
+++ R/model.R
@@ -1,25 +1,20 @@
 #' Build the models of the hydrological processes
 #'
-#' The function does the following:
-#'  -get the input parameters
-#' - build the models
-#' @param Timeresinsec time resolution of the process in second (1hour: 3600s, ... etc)
+#' Build the models of the hydrological processes
 #' @param ddd ddd object
+#' @param method "manual", "load","processedNVE"
+#' @param ... parameters related to the method
 #' @keywords ddd
 #' @export
 #' @examples
 #' \dontrun{
 #' model()
 #' }
-model <-function(Timeresinsec,ddd){
-
-  # GET PARAMETERS
-  ddd$inputParam$do("getParam",args=list(method=methodParam,path=pathParam, filename=fileParam))
-  ddd$inputParam$save(name="inputParam"))
+model <-function(ddd,method,...){
 
   # GET MODELS
-  ddd$model$do("getModel",args=list(method=methodModel,inputParam=inputParam,Timeresinsec=Timeresinsec)
-  ddd$model$save(name="model"))
+  ddd$model$do("getModel",args=list(method=method,...))
+  ddd$model$save(name="model")
 
   invisible()
 
