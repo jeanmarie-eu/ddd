fcea7c2f96619c915f863f014e5a71b29a7e357a
jeanmarielepioufle <jm@nexmodeling.com>
2016-11-18 14:37:27 +0100

    add temperature obs

--- R/obs.R
+++ R/obs.R
@@ -4,16 +4,21 @@
 #' @param ddd ddd object
 #' @param pathPrecip path of the precipitation observation
 #' @param filenamePrecip name of the precipitation file
+#' @param pathTemp path of the temperature observation
+#' @param filenameTemp name of the temperature file
 #' @keywords ddd
 #' @export
 #' @examples
 #' \dontrun{
 #' obs()
 #' }
-obs <-function(ddd,pathPrecip,filenamePrecip){
+obs <-function(ddd,pathPrecip,filenamePrecip,pathTemp,filenameTemp){
 
-  ddd$precipLZ$do("getPrecipLZ",args=list(method="none",path=pathPrecip,filename=filenamePrecip))
+  ddd$precipLZ$do("getPrecipLZ",args=list(method="processedNVE",path=pathPrecip,filename=filenamePrecip))
   ddd$precipLZ$save(name="obs")
+
+  ddd$tempLZ$do("getTempLZ",args=list(method="processedNVE",path=pathTemp,filename=filenameTemp))
+  ddd$tempLZ$save(name="obs")
 
   invisible()
 
