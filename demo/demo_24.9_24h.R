7fbd12256a8ffddbd9e03f14d6522cead40363a1
jeanmarielepioufle <jm@nexmodeling.com>
2016-11-18 18:41:53 +0100

    update demo

--- demo/demo_24.9_24h.R
+++ demo/demo_24.9_24h.R
@@ -2,7 +2,9 @@ rm(list=ls())
 
 library(ddd)
 
-ddd(fromPeriod="2000090106",toPeriod="2014123106",timeResolution="daily",catchment="Tingvatn",
+ddd<- start(namefield="ddd",namespace="ddd",pathRes="~/test/")
+
+main(ddd=ddd,fromPeriod="2000090106",toPeriod="2014123106",timeResolution="daily",catchment="Tingvatn",
     pathData=paste0(.libPaths()[1],"/ddd/data/"),
     fileData="val_24.9_24hptq_kal.txt",
     pathParam=paste0(.libPaths()[1],"/ddd/data/"),
