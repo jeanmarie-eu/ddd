e72b497869856da2237d78a8f5481bb7d435b96e
jeanmarielepioufle <jm@nexmodeling.com>
2016-11-18 12:36:51 +0100

    add a test script

---
+++ demo/test.R
@@ -0,0 +1,33 @@
+rm(list=ls())
+
+library(ddd)
+
+ddd<- start(namefield="ddd",namespace="ddd",pathRes="~/")
+
+timePeriod <- date(timeResolution="daily",fromPeriod="2000090106",toPeriod="2014123106",format="YY,MM,DD,HH",saveDate=NULL)
+
+pathData=paste0(.libPaths()[1],"/ddd/data/")
+fileData="val_24.9_24hptq_kal.txt"
+pathData <- normalizePath(file.path(pathData),mustWork = FALSE)
+tmp <- utils::read.table(normalizePath(file.path(pathData,fileData),mustWork = FALSE),sep="\t")
+precip <- as.matrix(tmp[,5:14])
+temp <- as.matrix(tmp[,15:24])
+q <- as.vector(tmp[,25])
+missingValues <- -10000
+q[q==missingValues] <- NA
+scaob   <- NA
+
+pathParam=paste0(.libPaths()[1],"/ddd/data/")
+fileParam="best_par_24.9_24h.txt"
+param(ddd=ddd,method="processedNVE",path=pathParam,filename=fileParam)
+
+model(ddd=ddd,method="processedNVE",Timeresinsec=timePeriod$Timeresinsec,inputParam=ddd$inputParam$values())
+
+init(Timeresinsec=timePeriod$Timeresinsec,ddd=ddd)
+
+ddd(fromPeriod="2000090106",toPeriod="2014123106",timeResolution="daily",catchment="Tingvatn",
+    pathData=paste0(.libPaths()[1],"/ddd/data/"),
+    fileData="val_24.9_24hptq_kal.txt",
+    pathParam=paste0(.libPaths()[1],"/ddd/data/"),
+    fileParam="best_par_24.9_24h.txt",
+    FIGURE=TRUE)
